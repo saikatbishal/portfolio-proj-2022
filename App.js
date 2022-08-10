a897407c50e1e83d7b16f8e83040e5b28d5a1a8a
subratabishal <subratabishal10@gmail.com>
2022-08-10 23:28:35 +0530

    added App.js
    Button functionality pending

---
+++ App.js
@@ -0,0 +1,8 @@
+const clickEventListener = () => {
+    const btn = document.createElement('button');
+    document.body.appendChild(btn);
+    btn.classList.add('btn-primary');
+    btn.addEventListener('click', () => {
+        console.log('click event encountered');
+    });
+}
